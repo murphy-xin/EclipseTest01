52940aa3eff91bb952f135bfdda591a9c55ac36d
murphy <xinmingfang1987@163.com>
2019-12-31 15:14:12 +0800

    git test02 commit

---
+++ gittest02/src/com/bjpn/gittest02/GitTest01.java
@@ -0,0 +1,11 @@
+package com.bjpn.gittest02;
+
+public class GitTest01 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("gittest01");
+		System.out.println("test01 sec commit");
+	}
+
+}
---
+++ gittest02/src/com/bjpn/gittest02/GitTest02.java
@@ -0,0 +1,8 @@
+package com.bjpn.gittest02;
+
+public class GitTest02 {
+	public static void main(String args[]) {
+		
+		System.out.println("gittest02");
+	}
+}
